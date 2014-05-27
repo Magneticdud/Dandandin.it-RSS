70dd7f99a1e5bfc6b7bfdf8172f6777ff2b8089e
Magnetic_dud <mail@dandandin.it>
2014-05-27 15:19:48 +0200

    well... this app was ready 3 years ago, so basically there's no history now

---
+++ src/it/dandandin/rss/DandandinRSSActivity.java
@@ -0,0 +1,13 @@
+package it.dandandin.rss;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DandandinRSSActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
